0ddec6e508a85b3cd5e6b97bdba79b8163829868
tonarinohito <rsp44316@gmail.com>
2018-01-05 02:06:23 +0900

    ロゴとボタンの追加

--- cookingVer1.3/cooking/Title.cpp
+++ cookingVer1.3/cooking/Title.cpp
@@ -3,17 +3,18 @@
 bool Title::Initialize()
 {
 	sound.Initialize();
-	sound.InitBGM();	//あほ
+	sound.InitBGM();	
 	sound.SetBGM("./Sound/いらっしゃいませ!.ogg");
 	α = 255 / 3;
 	startFlag = false;
 	endFlag = false;
 	int c[5];
-	c[0] = bgHandle = LoadGraph("./Graph/背景(仮).png");
-	c[1] = logo.handle = LoadGraph("./Graph/ロゴ(仮).png");
-	c[2] = s_button.handle = LoadGraph("./Graph/スタート(仮).png");
-	c[3] = e_button.handle = LoadGraph("./Graph/エンド(仮).png");
+	c[0] = bgHandle = LoadGraph("./Graph/bg_title.png");
+	c[1] = logo.handle = LoadGraph("./Graph/logo.png");
+	c[2] = s_button.handle = LoadGraph("./Graph/start_button.png");
+	c[3] = e_button.handle = LoadGraph("./Graph/exit_button.png");
 	c[4] = cursor.handle = LoadGraph("./Graph/カーソル(仮).png");
+
 	for (int i = 0; i < 5; ++i)
 	{
 		if (c[i] == -1)
@@ -22,8 +23,8 @@ bool Title::Initialize()
 		}
 	}
 	logo.pos.SetPOS(480, -300);
-	s_button.pos.SetPOS(350, 600);
-	e_button.pos.SetPOS(350, 700);
+	s_button.pos.SetPOS(310, 600);
+	e_button.pos.SetPOS(310, 700);
 	cursor.pos.SetPOS(-5000, 0);
 	cursor.select = Start;
 	return true;
@@ -32,13 +33,13 @@ bool Title::Initialize()
 void Title::Update()
 {
 	sound.PlayBGM_LOOP();
-	float l_dy = (100.f - logo.pos.y) / 20.0f;	//フワッとロゴを移動させる
+	float l_dy = (150.f - logo.pos.y) / 20.0f;	//フワッとロゴを移動させる
 	logo.pos.y += l_dy;
 	float s_dy = (300.f - s_button.pos.y) / 20.0f;
 	s_button.pos.y += s_dy;
 	float e_dy = (400.f - e_button.pos.y) / 22.0f;
 	e_button.pos.y += e_dy;
-	float c_dx = (290.f - cursor.pos.x) / 15.0f;
+	float c_dx = (260.f - cursor.pos.x) / 15.0f;
 	cursor.pos.x += c_dx;
 	if (startFlag == false && endFlag == false)
 	{
@@ -53,16 +54,17 @@ void Title::Update()
 		switch (cursor.select)
 		{
 		case Start:
-			cursor.pos.y = s_button.pos.y +10;
+			cursor.pos.y = s_button.pos.y +20;
 			break;
 		case End:
-			cursor.pos.y = e_button.pos.y +10;
+			cursor.pos.y = e_button.pos.y +20;
 			break;
 		}
 	}
 	if (α < 0 && endFlag == false)
 	{
 		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);		//ブレンドモードをオフ
+		SetDrawMode(DX_DRAWMODE_NEAREST);	//解除
 		SceneManeger::GetInstance()->ChangeScene(new OP);
 	}
 
@@ -99,8 +101,9 @@ void Title::Update()
 void Title::Draw()
 {
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, α);		//ブレンドモードαを設定
+	SetDrawMode(DX_DRAWMODE_BILINEAR);				//実数ピクセル補間
 	DrawGraph(0, 0, bgHandle, true);
-	DrawRotaGraphF(logo.pos.x, logo.pos.y, 1, 0, logo.handle, true);
+	DrawRotaGraphF(logo.pos.x, logo.pos.y, 1.2f, 0, logo.handle, true);
 	DrawGraphF(s_button.pos.x, s_button.pos.y, s_button.handle, true);
 	DrawGraphF(e_button.pos.x, e_button.pos.y, e_button.handle, true);
 	DrawGraphF(cursor.pos.x, cursor.pos.y, cursor.handle, true);
